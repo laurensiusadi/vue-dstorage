7007741ecd0fbd56e2f426b544c0e59c1de91382
Laurensius Adi <laurensiusadi@gmail.com>
2021-04-17 17:51:26 +0700

    DStorage contract

--- contracts/DStorage.sol
+++ contracts/DStorage.sol
@@ -3,33 +3,49 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract DStorage {
   string public name = 'DStorage';
-  // Number of files
-  // Mapping fileId=>Struct 
+  uint public fileCount = 0;
+  mapping(uint => File) public files;
 
-  // Struct
+  struct File {
+    uint fileId;
+    string fileHash;
+    uint filesSize;
+    string fileType;
+    string fileName;
+    string fileDescription;
+    uint uploadTime;
+    address uploader;
+  }
 
+  event FileUploaded(
+    uint fileId,
+    string fileHash,
+    uint filesSize,
+    string fileType,
+    string fileName,
+    string fileDescription,
+    uint uploadTime,
+    address uploader
+  );
 
-  // Event
+  function uploadFile(
+    string memory _fileHash,
+    uint _fileSize,
+    string memory _fileType,
+    string memory _fileName,
+    string memory _fileDescription
+  ) public {
+    require(bytes(_fileHash).length > 0);
+    require(bytes(_fileType).length > 0);
+    require(bytes(_fileName).length > 0);
+    require(bytes(_fileDescription).length > 0);
+    require(msg.sender!=address(0));
+    require(_fileSize>0);
 
-  // Upload File function
+    fileCount ++;
+    files[fileCount] = File(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
 
-    // Make sure the file hash exists
-
-    // Make sure file type exists
-
-    // Make sure file description exists
-
-    // Make sure file fileName exists
-
-    // Make sure uploader address exists
-
-    // Make sure file size is more than 0
-
-
-    // Increment file id
-
-    // Add File to the contract
-
-    // Trigger an event
+    emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, msg.sender);
+  }
 
 }
