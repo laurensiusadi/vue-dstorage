c60b33b6429ae7a26c3c94464e8d7a0913e04c75
Laurensius Adi <laurensiusadi@gmail.com>
2021-04-17 17:31:15 +0700

    Init truffle, add contracts and migrations

---
+++ contracts/DStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DStorage {
+  string public name = 'DStorage';
+  // Number of files
+  // Mapping fileId=>Struct 
+
+  // Struct
+
+
+  // Event
+
+  // Upload File function
+
+    // Make sure the file hash exists
+
+    // Make sure file type exists
+
+    // Make sure file description exists
+
+    // Make sure file fileName exists
+
+    // Make sure uploader address exists
+
+    // Make sure file size is more than 0
+
+
+    // Increment file id
+
+    // Add File to the contract
+
+    // Trigger an event
+
+}
