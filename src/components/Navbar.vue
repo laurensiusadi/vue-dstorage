78c994d45d573143859d9fddd8b0dc0cc58490b4
Laurensius Adi <laurensiusadi@gmail.com>
2021-04-17 22:23:04 +0700

    Connect to web3 and load

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template functional>
+  <nav class="navbar navbar-dark bg-dark p-0 text-monospace">
+    <a
+      class="navbar-brand col-sm-3 col-md-2 mr-0"
+      href="http://www.dappuniversity.com/bootcamp"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img src="../assets/box.png" width="30" height="30" class="align-top" alt="" />
+      D$t0r@g3
+    </a>
+    <ul class="navbar-nav px-3">
+      <li>
+        <small id="account">
+          <a target="_blank"
+              alt=""
+              class="text-white"
+              rel="noopener noreferrer"
+              :href="`https://etherscan.io/address/${props.account}`">
+            {{ props.account.substring(0,6) }}...{{ props.account.substring(38,42) }}
+          </a>
+        </small>
+      </li>
+    </ul>
+  </nav>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar :account="account"></navbar>
+    <div v-if="loading">Loading...</div>
+    <template v-else>
+      <div>Main</div>
+    </template>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Web3 from 'web3'
+import DStorage from '../abis/DStorage.json'
+import Navbar from '@/components/Navbar'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
+  },
+  data() {
+    return {
+      account: '',
+      dstorage: null,
+      files: [],
+      loading: false,
+      type: null,
+      name: null
+    }
+  },
+  created() {
+    this.loadWeb3()
+    this.loadBlockchainData()
+  },
+  methods: {
+    async loadWeb3() {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum)
+        await window.ethereum.enable()
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider)
+      } else {
+        window.alert('Non-ethereum browser detected. Try MetaMask.')
+      }
+    },
+    async loadBlockchainData() {
+      const web3 = window.web3
+
+      const accounts = await web3.eth.getAccounts()
+      this.account = accounts[0]
+
+      const networkId = await web3.eth.net.getId()
+      const networkData = DStorage.networks[networkId]
+      if (networkData) {
+        this.dstorage = new web3.eth.Contract(DStorage.abi, networkData.address)
+        this.filesCount = await this.dstorage.methods.fileCount().call()
+        for (var i = this.filesCount; i >= 1; i--) {
+          const file = await this.dstorage.methods.files(i).call()
+          this.files = [...this.files, file]
+        }
+      } else {
+        window.alert('DStorage contract not deployed to detected network.')
+      }
+    }
   }
 }
 </script>
